d75de8d61443c4ea3cb238601d6f9dc57d1bd16f
Brandon <verv0022@algoquinlive.com>
2019-04-22 05:39:21 -0400

    sign up page html

--- js/main.js
+++ js/main.js
@@ -4,20 +4,30 @@ let show = new Event('show');
 
 document.addEventListener("DOMContentLoaded", init);
 
+
 function init() {
-    pages = document.querySelectorAll('.page');
-        pages.forEach((pg)=>{
-            pg.addEventListener('show', pageShown);
-        })
-        
-        document.querySelectorAll('.nav-link').forEach((link)=>{
-            link.addEventListener('click', navigate);
-        })
-        history.replaceState({}, 'Home', '#home');
-        window.addEventListener('popstate', poppin);
+    pageSwitch();
+    addListeners();
+}
+
+function addListeners() {
+    // document.querySelector('.confirmBtn').addEventListener('click', doLogin);
 }
 
 
+
+function pageSwitch() {
+    pages = document.querySelectorAll('.page');
+    pages.forEach((pg) => {
+        pg.addEventListener('show', pageShown);
+    })
+
+    document.querySelectorAll('.nav-link').forEach((link) => {
+        link.addEventListener('click', navigate);
+    })
+    history.replaceState({}, 'Home', '#homePage');
+    window.addEventListener('popstate', poppin);
+}
 
 function navigate(ev) {
     ev.preventDefault();
@@ -27,42 +37,39 @@ function navigate(ev) {
     console.log(currentPage)
     history.pushState({}, currentPage, `#${currentPage}`);
     document.getElementById(currentPage).dispatchEvent(show);
-    }
+}
 
-function pageShown(ev){
+function pageShown(ev) {
     console.log('Page', ev.target.id, 'just shown');
     let h1 = ev.target.querySelector('h1');
     h1.classList.add('big')
-    setTimeout((h)=>{
+    setTimeout((h) => {
         h.classList.remove('big');
     }, 1200, h1);
 }
 
-function poppin(ev){
+function poppin(ev) {
 
     console.log(location.hash, 'popstate event');
-        let hash = location.hash.replace('#' ,'');
-        document.querySelector('.display').classList.remove('display');
-        document.getElementById(hash).classList.add('display');
-        console.log(hash)
-        history.pushState({}, currentPage, `#${currentPage}`);
-        document.getElementById(hash).dispatchEvent(show);
+    let hash = location.hash.replace('#', '');
+    document.querySelector('.display').classList.remove('display');
+    document.getElementById(hash).classList.add('display');
+    console.log(hash)
+    history.pushState({}, currentPage, `#${currentPage}`);
+    document.getElementById(hash).dispatchEvent(show);
 }
 
 
 
 
 /********************************* MODAL *********************************/
-let btn = document.querySelector('.signInBtn'),
+let signInBtn = document.querySelector('.signInBtn'),
     modal = document.querySelector('.modal'),
     closeBtn = document.querySelector('.closeBtn');
     signUpBtn = document.querySelector('.signUpBtn');
 
-//    signUpBtn.addEventListener('click', function () {
-//         modal.style.display = "none";
-//     })
 
-btn.addEventListener('click', function () {
+signInBtn.addEventListener('click', function () {
     modal.style.display = 'flex';
     console.log("clicked");
 })
@@ -75,4 +82,8 @@ window.addEventListener('click', function (e) {
     if (e.target == modal) {
         modal.style.display = "none";
     }
+})
+
+signUpBtn.addEventListener('click', function () {
+    modal.style.display = "none";
 })
